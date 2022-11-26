8dac0ece64446a5c69267b6b786a01d7457a18c0
alfinanggara <alfinanggara.mic@gmail.com>
2022-11-26 16:07:50 +0700

    tgl 26 nov

---
+++ database/migrations/2022_11_26_070551_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('location_id')->nullable();
+            $table->foreign('location_id')->references('id')->on('location');
+            $table->string('room_number')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rooms');
+    }
+}
